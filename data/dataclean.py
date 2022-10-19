73929e79748a83721e7bb2bfad041899e151c0e9
Christian Alexander Hetling <74573287+Hetling@users.noreply.github.com>
2022-10-19 16:58:07 +0200

    to csv

--- data/dataclean.py
+++ data/dataclean.py
@@ -4,15 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def main():
+  df = pd.read_excel('AllfileBig.xlsx', sheet_name='in')
+  df.index = df[0]
+  df = df.iloc[: , 2:]
 
-df = pd.read_excel('AllfileBig.xlsx', sheet_name='in')
-df.index = df[0]
-df = df.iloc[: , 2:]
-df.head()
 
+  # remove days in which all are nan
+  shape = df.shape
+  df.dropna(inplace=True, axis=0, how='all')
+  print(f'df is {shape}   Shape after dropping all NA columns: {df.shape}')
+  df.to_csv('AllfileBig.csv')
 
-# remove days in which all are nan
-shape = df.shape
-df.dropna(inplace=True, axis=0, how='all')
-print(f'df is {shape}   Shape after dropping all NA columns: {df.shape}')
-
+if __name__ == '__main__':
+  main()
