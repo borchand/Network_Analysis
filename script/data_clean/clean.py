67e285995ecd5a73919d27986dd6b5e45efb7f8f
borch_and <andersen_sebastian@yahoo.dk>
2022-11-10 15:08:46 +0100

    Update clean and remove deprecated files
    Co-Authored-By: Alexander Winther Nielsen <93655538+AlexWinther@users.noreply.github.com>
    Co-Authored-By: Malthe Have Musaeus <33017424+Malthehave@users.noreply.github.com>
    Co-Authored-By: ErlingAmundsen <94839025+ErlingAmundsen@users.noreply.github.com>
    Co-Authored-By: Christian Alexander Hetling <74573287+Hetling@users.noreply.github.com>

--- script/data_clean/clean.py
+++ script/data_clean/clean.py
@@ -2,7 +2,27 @@ import pandas as pd
 import glob
 from tqdm import tqdm
 
-def merge_data_to_csv(datasets):
+def find_ETFs(datasets):
+    x = set()
+    print("find ETF's...")
+    for dataset in datasets:
+        print("Scaning %s" % dataset)
+        all_files = glob.glob(f"./data/stock_market_data/{dataset}/json/*.json")
+
+        for file in tqdm(all_files):
+            with open(file, 'r') as f:
+                lines = [f.readline() for _ in range(10)]
+                line = lines[8].split("\"")[3]
+                symbol = lines[6].split("\"")[3]
+                time = lines[9].split(":")[1].split(",")[0]
+
+                if line != "EQUITY" or time == " null" or int(time) > 1420072583:
+                    x.add(symbol)
+    return x
+
+
+def merge_data_to_csv(datasets, etfs):
+
     dataframes = []
 
     for dataset in datasets:
@@ -13,8 +33,10 @@ def merge_data_to_csv(datasets):
         # For each file in all_files, read the csv file and append it to a list
         df_list = []
         for file in tqdm(all_files):
-            df = pd.read_csv(file)
-            df_list.append(df)
+            etf = file.split("/")[-1].split(".")[0]
+            if not etf in etfs:
+                df = pd.read_csv(file)
+                df_list.append(df)
 
         # Create a dataframe with all the data, including a column with the file name
         print("Concatenating %s" % dataset + "...")
@@ -28,50 +50,74 @@ def merge_data_to_csv(datasets):
         # Add the dataframe to a list
         dataframes.append(df)
 
-    print("Deleting duplicate tickers...")
+    print("Concatenating all...")
     # Load data
     all_df = pd.concat([dataframe for dataframe in dataframes])
+
+    print("Deleting duplicate tickers...")
     # Remove duplicate rows
     df = all_df.drop_duplicates()
-    # Download all_data
-    print("Saving to all_data.csv...")
-    df.to_csv('./data/stock_market_data/all_data.csv', index=False)
 
-def remove_ETFs(datasets):
-    x = set()
-    for dataset in datasets:
-        print("Scaning %s" % dataset)
-        all_files = glob.glob(f"./data/stock_market_data/{dataset}/json/*.json")
+    print("Remove delisted...")
+    nan_tickers = df[df['ticker'].isna()]['ticker'].unique()
 
-        for file in tqdm(all_files):
-            with open(file, 'r') as f:
-                lines = [f.readline() for _ in range(9)]
-                line = lines[8].split("\"")[3]
-                symbol = lines[6].split("\"")[3]
+    df = df[~df['ticker'].isin(nan_tickers)]
 
-                if line in ['ETF', 'MUTUALFUND']:
-                    x.add(symbol)
+    print("Printing to all_data.csv...")
+    # print df to csv
+    df.to_csv('./data/stock_market_data/all_data.csv')
+    return df
 
-    print("Total: %s" % len(x))
+def transform_data(stockdf):
+    print("Transform dataframe...")
+    if "Unnamed: 0" in stockdf.columns:
+        stockdf.drop(columns=['Unnamed: 0'], inplace=True)
+    # make date column into datetime object
+    stockdf['Date'] = pd.to_datetime(stockdf['Date'], dayfirst=True)
 
-    df = pd.read_csv('./data/stock_market_data/all_data.csv')
-    print("Length before removing ETFs: %s" % len(df))
-    df = df[~df['ticker'].isin(x)]
-    print("Length after removing ETFs: %s" % len(df))
+    #pivot table to get the stock price for each company
+    stockdf = stockdf.pivot_table(index='Date', columns='ticker', values='Close')
 
-    df.to_csv('./data/stock_market_data/equity_data.csv', index=False)
-    print("Data saved to equity_data.csv")
+    return stockdf
 
 
-def main():
+
+def adding_market_index(df):
+    print("Add market index...")
+    
+    print("Read HistoricalData_1667914714805.csv...")
+    ## Reading in the market_index and making sure that date is datetime so that easily mergable and that date is index
+    hist_data = pd.read_csv('./data/shared_data/HistoricalData_1667914714805.csv')
+    hist_data['Date'] = pd.to_datetime(hist_data['Date'])
+    hist_data.set_index('Date')
+
+    ## Join df with hist_data on Date
+    x = df.join(hist_data.set_index('Date')['Close/Last'], on='Date', how='left', lsuffix='_caller', rsuffix='_other').rename(columns={'Close/Last': 'COMP'})
+    ## formating column and naming stock COMP
+    return x
+
+def save_to_csv(stockdf):
+    print("Print to stockdf.csv...")
+    #print to csv
+    stockdf.to_csv('./data/stock_market_data/stockdf.csv')
+
+def clean_data(run_all=False):
     datasets = [
         "nyse",
         "nasdaq",
         "sp500",
         "forbes2000"
     ]
-    merge_data_to_csv(datasets)
-    remove_ETFs(datasets)
+    if run_all:
+        etfs = find_ETFs(datasets)
+        df = merge_data_to_csv(datasets, etfs)
+    else:
+        print("Reading all_data.csv...")
+        df = pd.read_csv('./data/stock_market_data/all_data.csv')
+
+    stockdf = transform_data(df)
+    stockdf = adding_market_index(stockdf)
+    save_to_csv(stockdf)
 
 if __name__ == '__main__':
-    main()
+    clean_data()
