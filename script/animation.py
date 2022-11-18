11bbff4fff37555553156754379cecebdbfb9b7d
Malthe Have Musaeus <malthehave@gmail.com>
2022-11-18 16:00:59 +0100

    Fixed animation

--- script/animation.py
+++ script/animation.py
@@ -21,13 +21,29 @@ stockdf = stockdf.iloc[::5]
 
 # Create subplot with network on top and small graph on bottom
 fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10, 10))
+# Make ax[1] smaller than ax[0]
+ax[1].set_position([.1, .1, .8, .2])
+# Make ax[0] take up the whole figure
+ax[0].set_position([.1, .3, .8, .6])
+# Remove the black border around the figure
+ax[0].set_axis_off()
+# Set border width on ax[1]
+ax[1].spines['top'].set_linewidth(0.5)
+ax[1].spines['right'].set_linewidth(0.5)
+ax[1].spines['bottom'].set_linewidth(0.5)
+ax[1].spines['left'].set_linewidth(0.5)
+# Plot initial data on ax[1] (bottom subplot)
+ax[1].plot(stockdf['COMP'], color='darkblue', linewidth=0.8)
 
-G = nx.read_gexf('./data/stockcorr_t0.9.gexf')
-# Get only first 10 nodes
+
+thresh = 0.9
+G = nx.read_gexf(f'./data/stockcorr_t{thresh}.gexf')
+# Get only first x nodes
 G = G.subgraph(list(G.nodes)[:100])
 pos = nx.spring_layout(G)
 
 # Draw edges
+# TODO: Redraw edges every frame depending on correlation for that week?
 nx.draw_networkx_edges(G, pos=pos, alpha=.1, ax=ax[0])
 # Draw labels
 nx.draw_networkx_labels(G, pos=pos, font_size=7, ax=ax[0])
@@ -37,9 +53,8 @@ red2gray = list(Color('red').range_to(Color('gray'), percentage_change_range))
 gray2green = list(Color('gray').range_to(Color('green'), percentage_change_range-5))
 
 def animate(week):
-    # fig.clear()
-    # Set title to week number
-    plt.title(f'Week {week+1}')
+    # Set title at top of plot to week number
+    fig.suptitle(f'Week {week+1} ({stockdf.index[week]})')
     # Get stock prices for this week
     stockprices = stockdf.iloc[week]
     # Get stock prices for last week
@@ -72,27 +87,24 @@ def animate(week):
             colors.append('#808080')
 
             
-        # Draw node
-    # nx.draw_networkx_nodes(G, pos=pos, nodelist=[node], node_color=color, node_size=1000, alpha=.8)
-
-    # Color nodes based on price change
-    # colors = []
-    # for node in G.nodes:
-    #     if stockprices[node] > last_stockprices[node]:
-    #         colors.append('g')
-    #     else:
-    #         colors.append('r')
-    
     # Draw nodes
     nx.draw_networkx_nodes(G, pos=pos, node_color=colors, node_size=20, ax=ax[0])
 
-    # PLot a line of mean_change
-    ax[1].plot(stockdf.index, stockdf['COMP'].to_numpy()[:week], color='black')
-    # ax[1].plot(week, mean_change, 'o', color='black')
+    # Plot current data on ax[1]
+    ax[1].lines[0].set_data(stockdf['COMP'][:week+1].index, stockdf['COMP'][:week+1])
+    # Plot dot on every week
+    ax[1].scatter(stockdf['COMP'][:week+1].index, stockdf['COMP'][:week+1], s=1, color='darkblue')
+    # Plot dot on future weeks
+    ax[1].scatter(stockdf['COMP'][week+1:].index, stockdf['COMP'][week+1:], s=1, color='lightgray')
 
-    # nx.draw(G, with_labels=True, pos=pos, node_color=colors)
+    # Plot COMP stock with green line if price increased and red if it decreased
+    # ax[1].plot(stockdf['COMP'][0:week+1], color='g' if stockprices['COMP'] > last_stockprices['COMP'] else 'r')
 
-    # Return figure
+    # Set y axis label
+    ax[1].set_ylabel('Overall Market (USD)')
+    # Tilt ax[1] x-labels
+    plt.setp(ax[1].get_xticklabels(), rotation=90,)
+
     return fig
 
 
