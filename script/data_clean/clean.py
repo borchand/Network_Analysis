489e489d3836315e366b679738d2a0b6e6b6132a
borch_and <andersen_sebastian@yahoo.dk>
2022-12-01 11:53:00 +0100

    Update clean.py

--- script/data_clean/clean.py
+++ script/data_clean/clean.py
@@ -43,69 +43,39 @@ def find_ETFs(datasets):
     return x
 
 
-def merge_data_to_csv(datasets, etfs):
-    cut_off_date = datetime.datetime(2005, 1, 1)
-    dataframes = []
-    tickers = set()
+def create_df(datasets, etfs):
+    #Create empty dataframe with daterange index in range
+    df = pd.DataFrame()
+    df.index = pd.date_range(start='2005-1-1',end= '2023-1-1') 
+
+
     for dataset in datasets:
-        print("Downloading %s" % dataset + "...")
+        print(f'reading JSON for {dataset}')
         # Get all files from /data/stock_market_data/dataset/csv
         all_files = glob.glob(f"../../data/stock_market_data/{dataset}/csv/*.csv")
 
-        # For each file in all_files, read the csv file and append it to a list
-        df_list = []
         for file in tqdm(all_files):
+
             symbol = file.split("/")[-1].split(".")[0]
-            if symbol not in etfs or symbol not in tickers:
-                df = pd.read_csv(file)
-                df["Date"] = pd.to_datetime(df["Date"], dayfirst=True)
-                # cut off data before datetime of 2005-01-01
-                df = df[df["Date"] >= cut_off_date]
-                df_list.append(df)
-                if symbol == 'AAPL':
-                    df['Close'].plot()
-                    plt.show()
-                tickers.add(symbol)
+            
+            #if not in etfs set merge to df
+            if symbol not in etfs:
+                #read csv file for corresponding symbol
+                temp_df = pd.read_csv(file)
 
-        # Create a dataframe with all the data, including a column with the file name
-        print("Concatenating %s" % dataset + "...")
-        df = pd.concat(df_list, keys=all_files)
+                #Set index of temp df to datetime and rename close column to symbol name (for merging)
+                temp_df.index = pd.to_datetime(temp_df['Date'], format='%d-%m-%Y')
+                temp_df.rename({'Close': symbol}, axis=1, inplace=True)
 
-        # Extract the ticker from the file name. The ticker is right before .csv
-        df['ticker'] = df.index.get_level_values(0).str.split('/').str[-1].str.split('.').str[0]
-        
-        # Reset the index
-        df = df.reset_index(drop=True)
-        # Add the dataframe to a list
-        dataframes.append(df)
+                #merge on index only from 2005
+                df = df.merge(temp_df[symbol]['2005-1-1':], left_index=True, right_index=True, how='left')
 
-    print("Concatenating all...")
-    # Load data
-    all_df = pd.concat([dataframe for dataframe in dataframes])
+    #remove days with 0 data ie. hollidays and weekends.
+    df.dropna(axis=0, how='all', inplace=True)
 
-    print("Remove delisted...")
-    nan_tickers = df[df['ticker'].isna()]['ticker'].unique()
-
-    df = df[~df['ticker'].isin(nan_tickers)]
-
-    print("Saving to all_data.csv...")
-    # print df to csv
-    df.to_csv('../../data/stock_market_data/all_data.csv')
+    #drop duplicate columns by finding columns where pandas appended _x or _y to avoid conflict
+    df.drop(list(df.filter(regex = '_')), axis = 1, inplace = True)
     return df
-
-
-def transform_data(stockdf):
-    print("Transform dataframe...")
-    if "Unnamed: 0" in stockdf.columns:
-        stockdf.drop(columns=['Unnamed: 0'], inplace=True)
-    # make date column into datetime object
-    stockdf['Date'] = pd.to_datetime(stockdf['Date'], dayfirst=True)
-
-    #pivot table to get the stock price for each company
-    stockdf = stockdf.pivot_table(index='Date', columns='ticker', values='Close')
-
-    return stockdf
-
 
 def adding_market_index(df):
     print("Add market index...")
@@ -121,16 +91,10 @@ def adding_market_index(df):
     ## formating column and naming stock COMP
     return x
 
-def save_to_csv(stockdf, fileName='stockdf'):
-    print(f"Saving to {fileName}.csv...")
-    #print to csv
-    stockdf.to_csv(f'../../data/stock_market_data/{fileName}.csv')
-
 def drop_nan_std(stockdf):
     # 0 std check, and removing them from stockdf
     drop_list = []
     for i in stockdf.columns:
-        std_ = np.std(stockdf[i])
         if np.std(stockdf[i]) == 0:
             print(f'{i} has a standard deviation of {np.std(stockdf[i])}')
             drop_list.append(i)
@@ -142,7 +106,7 @@ def sub_dataframe_to_csv(stockdf, start_date, end_date, fileName):
     ## create a mask where the date is between start and end
     print(f"Creating {fileName}.csv...")
     mask = (stockdf.index >= start_date) & (stockdf.index <= end_date)
-    save_to_csv(stockdf[mask].set_index(stockdf[mask].index), fileName)
+    stockdf[mask].set_index(stockdf[mask].index).to_csv(f'../../data/stock_market_data/{fileName}.csv')
     
 
 def clean_data(run_all=False):
@@ -155,31 +119,18 @@ def clean_data(run_all=False):
     if not run_all:
         try:
             print("Reading all_data.csv...")
-            df = pd.read_csv('../../data/stock_market_data/all_data.csv')
+            df = pd.read_csv('../../data/stock_market_data/stockdf.csv')
         except FileNotFoundError:
             run_all = True
 
     if run_all:
         etfs = find_ETFs(datasets)
-        df = merge_data_to_csv(datasets, etfs)
+        df = create_df(datasets, etfs)
 
-    stockdf = transform_data(df)
-    stockdf = adding_market_index(stockdf)
-    stockdf = drop_nan_std(stockdf)
-
-    # creat covid csv
-    start_of_corona = datetime.datetime(2020, 3, 15)
-    end_of_corona = datetime.datetime(2022, 1, 30)
-    
-    sub_dataframe_to_csv(stockdf, start_of_corona, end_of_corona, 'stockdf_corona')
-    
-    # creat ukrain war csv
-    start_of_ukrain = datetime.datetime(2022, 2, 24)
-    end_of_ukrain = datetime.datetime(2022, 10, 24)
-    sub_dataframe_to_csv(stockdf, start_of_ukrain, end_of_ukrain, 'stockdf_ukrain')
-    
-    save_to_csv(stockdf)
-    print(f"Total amount of tickers after cleaning: {len(stockdf.columns)}")
+    print(f"Saving to stockdf.csv...")
+    #print to csv
+    df.to_csv(f'../../data/stock_market_data/stockdf.csv')
+    print(f"Total amount of tickers after cleaning: {len(df.columns)}")
 
 if __name__ == '__main__':
     print(platform)
