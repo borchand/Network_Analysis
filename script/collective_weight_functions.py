9f4af0174ff7a492c76158d38cf955d2f8f82cf5
ErlingAmundsen <94839025+ErlingAmundsen@users.noreply.github.com>
2022-12-08 14:47:12 +0100

    Update collective_weight_functions.py
    Co-Authored-By: borchand <andersen_sebastian@yahoo.dk>

--- script/collective_weight_functions.py
+++ script/collective_weight_functions.py
@@ -24,5 +24,80 @@ def split_into_years(thresh = 0.9):
 
     ## sum all correlation matrices in corr_list
 
-    total_sum = np.sum(corr_list)
+    
+    total_sum = np.add(corr_list)
     return corr_list, total_sum, dataframes_
+
+
+def soting_indexes(list_of_lists:list[list]):
+    intersects = []
+
+    for i in range(len(list_of_lists)):
+        for j in range(i+1,len(list_of_lists)):
+            set_i = set(list_of_lists[i])
+            set_j = set(list_of_lists[j])
+            intersect = set_i.intersection(set_j)
+            intersects.append([list(intersect),i,j])
+
+    idx_in_list = list(range(len(list_of_lists)))
+    # print(idx_in_list)
+    
+    # sort intersect by length of the first element in the list
+    intersects = sorted(intersects, key=lambda x: len(x[0]), reverse=True)
+    # print(intersects)
+
+
+    final_list = []
+    stored_idx = None
+    while idx_in_list != []:
+        if stored_idx == None:
+            final_list.append(list_of_lists[intersects[0][1]])
+            final_list.append(list_of_lists[intersects[0][2]])
+            if intersects[0][1] in idx_in_list:
+                idx_in_list.remove(intersects[0][1])
+            if intersects[0][2] in idx_in_list:
+                idx_in_list.remove(intersects[0][2])
+            stored_idx = intersects[0][2]
+            intersects.pop(0)
+
+        else:
+            found = False
+            for i in range(len(intersects)):
+                if stored_idx == intersects[i][1]:
+                    final_list.append(list_of_lists[intersects[i][2]])
+                    if intersects[i][2] in idx_in_list:
+                        idx_in_list.remove(intersects[i][2])
+                    found = True
+                    break
+                
+                elif stored_idx == intersects[i][2]:
+                    final_list.append(list_of_lists[intersects[i][1]])
+                    if intersects[i][1] in idx_in_list:
+                        idx_in_list.remove(intersects[i][1])
+                    found = True
+
+                    intersects.pop(i)
+                    break
+            if not found:
+                stored_idx = None
+                    
+    # print(final_list)
+    ## flatten final_list
+    final_list = [item for sublist in final_list for item in sublist]
+
+    final_list = list(dict.fromkeys(final_list))
+
+    return final_list
+
+
+
+def main():
+    list_ = [[5,6,7,8,9,4,3], [1,2,3,4,9], [1,2,3,88], [11,12,13,14,10,9,8,7,1,2,3]]
+
+    soting_indexes(list_)
+
+if __name__ == '__main__':
+    main()
+
+
+        
