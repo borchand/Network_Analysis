bc2e8e31d6567c32a45a4c05d9adac243d6e2259
borch_and <andersen_sebastian@yahoo.dk>
2022-12-19 17:01:03 +0100

    Update newClean.py

--- script/data_clean/newClean.py
+++ script/data_clean/newClean.py
@@ -18,12 +18,12 @@ def get_data():
     min_year = df.index.min().year
     max_year = df.index.max().year
 
-    years = [df.loc[f'{year}-01-01':f'{year}-12-31'] for year in range(min_year, max_year + 1)]
+    df_years = [df.loc[f'{year}-01-01':f'{year}-12-31'] for year in range(min_year, max_year + 1)]
 
-    return df, years, min_year, max_year
+    return df_years, min_year, max_year
 
-def get_corr_from_year(df, year, years, min_year):
-    year_data = years[year - min_year]
+def get_corr_from_year(year, df_years, min_year):
+    year_data = df_years[year - min_year]
 
     # remove tickers that don't have any variation during the year
     year_variation = np.var(year_data, axis=0).astype('float16')
@@ -37,7 +37,7 @@ def get_corr_from_year(df, year, years, min_year):
     return corr_df
 
 def main():
-    df, years, min_year, max_year = get_data()
+    df_years, min_year, max_year = get_data()
 
     corr_df = get_corr_from_year(df, 2020, years, min_year)
     
